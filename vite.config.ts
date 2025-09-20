3e121a28be3ddb5cc8501b1eebe9584daa31ebce
Andrew <andrew.krohmalyuk12@gmail.com>
2025-09-21 02:07:44 +0300

    Bundle extension with hmr settings (#1)
    * added pnpm initial settings
    
    * setup build
    
    ---------
    
    Co-authored-by: Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import webExtPlugin from '@create-web-ext/hot-reload'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+		react(),
+		// @ts-ignore
+		webExtPlugin({ sourceDir: '/dist' }),
+	],
 })
